fd60e8cab8cf7bf6cd78f9012b70927ee176a544
salvador.fuentes <pit.091199@gmail.com>
2020-11-21 13:28:04 -0600

    Added file to explain list in python

---
+++ 01_listas.py
@@ -0,0 +1,18 @@
+#Archivo explicando listas
+
+numeros = [23, 89, 127, 1289, 8912]
+print(numeros)
+
+#Para agregar un elemento a una lista ocupamos append
+
+numero_extra= 33
+numero_extra_dos= 1233
+numero_extra_tres= 89
+
+numeros.append(numero_extra)
+print(numeros)
+
+#Para eliminar el ultimo elemento de la lista ocupamos "pop"
+numeros.pop()
+print(numeros) 
+
